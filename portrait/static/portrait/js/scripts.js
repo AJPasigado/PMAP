2dbc2f2f4896b147c1c393d2c34e3e6ab5338355
AJ Pasigado <ajpasigado@outlook.com>
2019-03-05 10:46:07 +0800

    Initial Commit

---
+++ portrait/static/portrait/js/scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#generate_btn").on("click", function () {
+        if (!$("#container").hasClass("show-pic")){
+            $("#container").addClass("show-pic");
+        } 
+    });
+});
