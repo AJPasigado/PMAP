e6029b1b787b7e796429ba0674107d2b7132e0f7
Justinhenrylo <lojustinhenry@gmail.com>
2019-03-06 23:59:26 +0800

    display pics

--- portrait/views.py
+++ portrait/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-
+from generator import generate
 # Create your views here.
 
 def index(request):
     context = {}
-    return render(request, 'index.html', context)
+    if(generate("model/g_[1400].h5","uploads/grayscale") and generate("model/g_[2000].h5","uploads/colored")):
+        return render(request, 'index.html', context)
