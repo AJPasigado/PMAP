8beaad94c110d43554dd116e9b6ffc4bdd5fad0d
Justinhenrylo <lojustinhenry@gmail.com>
2019-03-07 01:35:12 +0800

    buggy code

--- portrait/views.py
+++ portrait/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from generator import generate
+from generator import generate, generate_color
 # Create your views here.
 
 def index(request):
     context = {}
-    if(generate("model/g_[1400].h5","uploads/grayscale") and generate("model/g_[2000].h5","uploads/colored")):
+    #if(generate("model/g_[1400].h5","uploads/grayscale") and generate("model/g_[2000].h5","uploads/colored")):
+    if(generate_color()):
         return render(request, 'index.html', context)
